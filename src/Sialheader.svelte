f3e542c815b3f2f2e989b4c188fd9a5cd3168297
comexposium <hassan.akaou@comexposium.com>
2020-04-23 16:37:34 +0200

    add svg inside images folder

--- src/Sialheader.svelte
+++ src/Sialheader.svelte
@@ -1,6 +1,10 @@
+<script>
+ const svg = "/images/logo-sial.svg"
+</script>
+
 <nav class="flex items-center justify-center bg-yellow-400 flex-wrap p-6 border-b-2 border-solid border-grey">
   <div class="flex items-center flex-shrink-0 text-black mr-6">
-    <svg style="background: url(https://www.sialparis.fr/theme/sial_2019_desktop_git/img/logo-sial.svg) center center/contain no-repeat;" class="fill-current h-8 mr-2" width="100" height="100"></svg>
+    <svg style="background: url({svg}) center center/contain no-repeat;" class="fill-current h-8 mr-2" width="100" height="100"></svg>
     <span class="font-semibold text-l tracking-tight">18 - 22 october 2020 <br>
     <span class="text-m font-thin tracking-tight">PARIS NORD VILLEPINTE</span>
     </span>
